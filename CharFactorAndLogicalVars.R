0d227611780c9b7ca4ae872a2d805354a9ad4a7f
Miners000 <149023772+Miners000@users.noreply.github.com>
2025-08-01 01:40:04 -0400

    Chapter Two Exercise Two - Char, Factor, and Logical Vars in R

---
+++ CharFactorAndLogicalVars.R
@@ -0,0 +1,11 @@
+library(abdwr3edata)
+library(tidyverse)
+
+outcomes = c("Single", "Out", "Out", "Single", "Out", "Double", "Out", "Walk", "Out", "Single")
+table(outcomes)
+
+f.outcomes = factor(outcomes, levels = c("Out", "Walk", "Single", "Double"))
+table(f.outcomes)
+
+outcomes == "Walk"
+sum(outcomes == "Walk")
